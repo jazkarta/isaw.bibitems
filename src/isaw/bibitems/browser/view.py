a85bed688bacc1497d7442a2e62cdf2d50a236fd
Alec Mitchell <alecpm@gmail.com>
2016-09-12 18:20:30 -0700

    Refactor to use more efficient method for domain determination.

---
+++ src/isaw/bibitems/browser/view.py
@@ -0,0 +1,20 @@
+from urlparse import urlparse
+from Products.Five.browser import BrowserView
+
+
+class BibItemView(BrowserView):
+
+    @staticmethod
+    def url_domain(url):
+        host = urlparse(url).hostname
+        parts = host.split('.')
+        domain = []
+
+        for part in reversed(parts):
+            if part in ('www', 'api'):
+                continue
+            domain.insert(0, part)
+            if len(domain) >= 2 and len(part) > 3:
+                break
+
+        return '.'.join(domain)
